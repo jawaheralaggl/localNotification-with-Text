e69446f083026f4fb151d64bf8c6901bce2f3a50
jawaheralaggl <jawaheralaggl@gmail.com>
2020-07-24 15:11:03 +0300

    Initial Commit

---
+++ notificationWithText/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  notificationWithText
+//
+//  Created by Jawaher Alagel on 7/24/20.
+//  Copyright © 2020 Jawaher Alaggl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
