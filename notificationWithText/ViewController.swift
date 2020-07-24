40567743531b34a3b04f427625746514e7c70eff
jawaheralaggl <jawaheralaggl@gmail.com>
2020-07-24 16:23:14 +0300

    first commit

--- notificationWithText/ViewController.swift
+++ notificationWithText/ViewController.swift
@@ -9,12 +9,47 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
-
-
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBAction func setButton(_ sender: Any) {
+        
+        let center = UNUserNotificationCenter.current()
+        let content = UNMutableNotificationContent()
+        content.title = "You have New Message!"
+        content.body = "Check Now"
+        content.sound = .default
+        
+        
+        // Set text action to take input
+        let categoryIdentifier = "message.action"
+        let reply = UNTextInputNotificationAction(identifier: "reply.action", title: "Reply", options: [], textInputButtonTitle: "Send", textInputPlaceholder: "type...")
+        let cancel = UNNotificationAction(identifier: "cancel.action", title: "Cancel", options: [])
+        
+        let category = UNNotificationCategory(identifier: categoryIdentifier, actions: [reply, cancel], intentIdentifiers: [], options: [])
+        UNUserNotificationCenter.current().setNotificationCategories([category])
+        
+        content.categoryIdentifier = categoryIdentifier
+        
+        
+        
+        let notiDate = Calendar.current.dateComponents([.day, .month, .year, .hour, .minute, .second], from: Date().addingTimeInterval(10))
+        
+        let trigger = UNCalendarNotificationTrigger(dateMatching: notiDate, repeats: true)
+        let request = UNNotificationRequest(identifier: "reminder", content: content, trigger: trigger)
+        center.add(request) { (error) in
+            if error != nil {
+                print("error:\(error?.localizedDescription ?? "Error Local Notification" )")
+                
+            }
+        }
+        
+    }
+    
+    
 }
-
